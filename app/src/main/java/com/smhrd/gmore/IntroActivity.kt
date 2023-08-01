1ab69060fa80fd1f1efd23fa4cc15c0533bb0b91
김소희 <zz7854@naver.com>
2023-08-01 10:36:11 +0900

    .

--- app/src/main/java/com/smhrd/gmore/IntroActivity.kt
+++ app/src/main/java/com/smhrd/gmore/IntroActivity.kt
@@ -3,9 +3,10 @@ package com.smhrd.gmore
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity2 : AppCompatActivity() {
+class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_intro)
+
     }
 }
---
+++ app/src/main/java/com/smhrd/gmore/chat/ChattingRoomActivity.kt
@@ -0,0 +1,12 @@
+package com.smhrd.gmore.chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.smhrd.gmore.R
+
+class ChattingRoomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chatting_room)
+    }
+}
