538d231d1221b46894a195086bf0a10630c9fe4d
김다운 <128446489+kimdaun98@users.noreply.github.com>
2023-08-02 17:05:32 +0900

    update
    fragment_3.xml

---
+++ app/src/main/java/com/smhrd/gmore/fragment/Fragment3VO.kt
@@ -0,0 +1,12 @@
+package com.smhrd.gmore.fragment
+
+data class Fragment3VO (
+    val board_id: Int,
+    val user_id: Int,
+    val title: String,
+    val content: String,
+    val image_url: String,
+    val category: String,
+    val date_created: String
+)
+
