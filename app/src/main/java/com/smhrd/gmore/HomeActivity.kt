685881fc71791acb9eacabb2d7abbb14fca05cad
이현성 <528abcdef@naver.com>
2023-08-02 11:14:28 +0900

    1

--- app/src/main/java/com/smhrd/gmore/HomeActivity.kt
+++ app/src/main/java/com/smhrd/gmore/HomeActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.smhrd.gmore.board.GameCategoryActivity
 
 class HomeActivity : AppCompatActivity() {
 
@@ -18,9 +19,15 @@ class HomeActivity : AppCompatActivity() {
         btn2 = findViewById(R.id.btn2)
 
         btn1.setOnClickListener {
-//            var it_next: Intent =
-//                Intent(this, LoginActivity::class.java) // 현재 엑티비티(첫번째 메인), 두번째 받는 사람
-//            startActivity(it_next)
+            var it_next: Intent =
+                Intent(this, GameCategoryActivity::class.java) // 현재 엑티비티(첫번째 메인), 두번째 받는 사람
+            startActivity(it_next)
+        }
+
+        btn2.setOnClickListener {
+            var it_next: Intent =
+                Intent(this, GameCategoryActivity::class.java) // 현재 엑티비티(첫번째 메인), 두번째 받는 사람
+            startActivity(it_next)
         }
 
 
