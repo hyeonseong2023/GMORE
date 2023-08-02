1e5cbe0d064805ffe33dfae78473118ba6e10f35
김소희 <zz7854@naver.com>
2023-08-02 17:39:59 +0900

    node

--- Gnode/app.js
+++ Gnode/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
-const indexRouter = require('./routes/board');
-const favoritesRouter = require('./routes/favorites'); // 대/소문자를 일치시키세요
+const boardRouter = require('./routes/board')
+const memberRouter = require('./routes/member')
+const indexRouter = require('./routes')
+const favoritesRouter = require('./routes/favorites')
 
 app.use(express.urlencoded({ extended: true }));
-app.use('/board', indexRouter);
+app.use('/board', boardRouter);
+app.use('/user', indexRouter);
 app.use('/favorites', favoritesRouter); // 수정된 변수를 사용하세요
+app.use('/member', memberRouter)
 app.set('port', process.env.PORT || 8888);
 app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 서버연결 기다리는 중...');
